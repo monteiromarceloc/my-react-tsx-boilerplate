a15d798e1974db0ad9bb6105f63a96f814fc520c
monteiromarceloc <monteiromarceloc@gmail.com>
2019-11-28 15:55:47 -0300

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+type AppProps = {
+  iframeParam?: string,
+}
+
+const App: React.FC<AppProps> = (props) => {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Param: <code className="code">{props.iframeParam || 'nenhum parâmetro recebido'}</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+type AppProps = {
+    iframeParam: string,
+}
+
+// GET IFRAME PARAM
+let getParamValue = function(paramName: string)
+{
+    var url = window.location.search.substring(1); //get rid of "?" in querystring
+    var qArray = url.split('&'); //get key-value pairs
+    for (var i = 0; i < qArray.length; i++) 
+    {
+        var pArr = qArray[i].split('='); //split key and value
+        if (pArr[0] === paramName) 
+            console.log(pArr[1]);
+            return pArr[1]; //return value
+    }
+}
+
+const userToken = getParamValue('userToken');
+
+ReactDOM.render(<App iframeParam={userToken} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
