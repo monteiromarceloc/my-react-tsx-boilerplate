8d06df9806673f60b5405f79c9d8f7fe8577fe9d
monteiromarceloc <monteiromarceloc@gmail.com>
2019-11-28 16:06:28 -0300

    having fun

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,32 @@ type AppProps = {
   iframeParam?: string,
 }
 
-const App: React.FC<AppProps> = (props) => {
+// GET IFRAME PARAM
+let getParamValue = function(paramName: string)
+{
+    var url = window.location.search.substring(1); //get rid of "?" in querystring
+    var qArray = url.split('&'); //get key-value pairs
+    for (var i = 0; i < qArray.length; i++) 
+    {
+        var pArr = qArray[i].split('='); //split key and value
+        if (pArr[0] === paramName)
+            console.log(pArr[1]);
+            return pArr[1]; //return value
+    }
+}
 
+const App: React.FC<AppProps> = () => {
+  console.log(getParamValue('userToken'))
+  const iframeParam= getParamValue('userToken');
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Param: <code className="code">{props.iframeParam || 'nenhum parâmetro recebido'}</code>
+          Param: <code className="code">{iframeParam || 'nenhum parâmetro recebido'}</code>
         </p>
+
+        <iframe src="https://master.d2eo661bcmn9ya.amplifyapp.com/?userToken=123456789" />
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,27 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-type AppProps = {
-    iframeParam: string,
-}
 
-// GET IFRAME PARAM
-let getParamValue = function(paramName: string)
-{
-    var url = window.location.search.substring(1); //get rid of "?" in querystring
-    var qArray = url.split('&'); //get key-value pairs
-    for (var i = 0; i < qArray.length; i++) 
-    {
-        var pArr = qArray[i].split('='); //split key and value
-        if (pArr[0] === paramName) 
-            console.log(pArr[1]);
-            return pArr[1]; //return value
-    }
-}
-
-const userToken = getParamValue('userToken');
-
-ReactDOM.render(<App iframeParam={userToken} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
